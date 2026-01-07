244f2e889020f0cd43c953641616c6ca33b9e955
karwowski1 <131033750+karwowski1@users.noreply.github.com>
2026-01-07 10:46:43 +0100

    Updated paths

--- task-2-terraform/terraform.tfvars
+++
@@ -1,3 +0,0 @@
-
-root_bucket_name = "karwo-bucket-for-staticwebsite"
-root_project_name = "static-website-part2"
--- task-2-terraform/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "root_bucket_name" {
-  type        = string
-  description = "Nazwa bucketa dla strony"
-}
-
-variable "root_project_name" {
-  type        = string
-  default     = "static-website-terraform"
-}
