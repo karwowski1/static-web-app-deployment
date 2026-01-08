5c980aeceaf41b3033134ddc48ad7c3a37d34015
karwowski1 <131033750+karwowski1@users.noreply.github.com>
2026-01-08 17:00:57 +0100

    Add OAC

--- Static-v2/envs/dev/main.tf
+++ Static-v2/envs/dev/main.tf
@@ -24,3 +24,20 @@ module "cloudfront" {
     Environment = "Dev"
   }
 }
+
+resource "aws_s3_bucket_policy" "bucket_policy" {
+  bucket = module.s3_website.bucket_id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid = "AllowCloudFrontAccess"
+        Effect = "Allow"
+        Principal = { Service = "cloudfront.amazonaws.com" }
+        Action = "s3:GetObject"
+        Resource = "${module.s3_website.bucket_arn}/*"
+      }
+    ]
+  })
+}
