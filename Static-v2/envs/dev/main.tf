a784673740c479a237920718332ac47b3b12d8c6
karwowski1 <131033750+karwowski1@users.noreply.github.com>
2026-01-12 13:09:54 +0100

    Fix terraform formatting in envs/dev

--- Static-v2/envs/dev/main.tf
+++ Static-v2/envs/dev/main.tf
@@ -1,23 +1,23 @@
 module "s3_website" {
-  source   = "../../modules/s3"
-  s3_name  = "s3-static-app-v2"
-  tags     = {
+  source  = "../../modules/s3"
+  s3_name = "s3-static-app-v2"
+  tags = {
     Name        = "Static-app"
     Environment = "Dev"
   }
 }
 
 module "cloudfront" {
-  source               = "../../modules/cloudfront"
-  origin_id            = module.s3_website.bucket_id
-  bucket_domain_name   = module.s3_website.bucket_domain_name
-  waf_acl_id           = module.waf.waf_acl_arn
+  source                = "../../modules/cloudfront"
+  origin_id             = module.s3_website.bucket_id
+  bucket_domain_name    = module.s3_website.bucket_domain_name
+  waf_acl_id            = module.waf.waf_acl_arn
   logging_bucket_domain = module.s3_website.logs_bucket_domain_name
   tags = {
     Name        = "Static-app-CF"
     Environment = "Dev"
   }
-  
+
 }
 
 
@@ -30,15 +30,15 @@ module "waf" {
     Name        = "Static-app-WAF-Dev"
     Environment = "Dev"
   }
-  
+
 }
 
 module "cloudwatch" {
   source = "../../modules/cloudwatch"
-  
-  env = "dev"
+
+  env                        = "dev"
   cloudfront_distribution_id = module.cloudfront.cloudfront_distribution_id
-  waf_name = module.waf.aws_waf_name
+  waf_name                   = module.waf.aws_waf_name
 }
 
 
