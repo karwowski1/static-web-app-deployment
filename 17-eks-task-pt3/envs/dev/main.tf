c1cfa506720c944fbe03c092c3a2d57631362a32
karwowski1 <131033750+karwowski1@users.noreply.github.com>
2026-02-11 12:08:17 +0100

    Added bastion host

--- 17-eks-task-pt3/envs/dev/main.tf
+++ 17-eks-task-pt3/envs/dev/main.tf
@@ -14,9 +14,11 @@ module "vpc" {
 module "eks" {
   source = "../../modules/eks"
 
-  cluster_name = var.cluster_name
-  subnet_ids   = module.vpc.public_subnet_ids
+  cluster_name       = var.cluster_name
+  subnet_ids         = module.vpc.public_subnet_ids
   private_subnet_ids = module.vpc.private_subnet_ids
+  bastion_role_arn   = module.bastion.bastion_role_arn
+  bastion_security_group_id = module.bastion.bastion_security_group_id
 }
 
 module "bastion" {
@@ -27,3 +29,4 @@ module "bastion" {
   vpc_id       = module.vpc.vpc_id
   subnet_id    = module.vpc.private_subnet_ids[0]
 }
+
--- 17-eks-task-pt3/modules/eks/variables.tf
+++ 17-eks-task-pt3/modules/eks/variables.tf
@@ -10,3 +10,13 @@ variable "subnet_ids" {
 variable "private_subnet_ids" {
   type = list(string)
 }
+
+variable "bastion_role_arn" {
+  description = "IAM Role ARN of the Bastion Host to allow cluster access"
+  type        = string
+}
+
+variable "bastion_security_group_id" {
+  description = "Security Group ID of the Bastion Host to allow HTTPS access"
+  type        = string
+}
