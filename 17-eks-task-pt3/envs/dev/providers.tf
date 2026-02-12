8325eba22a730d74dd576ccaca6b8d37b37355e1
karwowski1 <131033750+karwowski1@users.noreply.github.com>
2026-02-12 09:24:40 +0100

    final infra4

--- 17-eks-task-pt3/envs/dev/providers.tf
+++ 17-eks-task-pt3/envs/dev/providers.tf
@@ -9,7 +9,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket  = "eks-task-tf-state-pt3"
+    bucket  = "eks-task2-tf-state"
     key     = "envs/dev/terraform.tfstate"
     region  = "eu-central-1"
     encrypt = true
