49c4678100a97498cab502aa0a6ea338c34bae2a
karwowski1 <131033750+karwowski1@users.noreply.github.com>
2026-04-22 15:49:49 +0200

    fix: path to module app cluster

--- 40-jenkins-pt4/terraform/envs/dev/main.tf
+++ 40-jenkins-pt4/terraform/envs/dev/main.tf
@@ -11,7 +11,7 @@ module "vpc" {
 }
 
 module "app_cluster" {
-  source          = "../modules/app_cluster"
+  source          = "../../modules/app_cluster"
   environment     = "dev"
   vpc_id          = module.vpc.vpc_id
   public_subnets  = module.vpc.public_subnet_ids
