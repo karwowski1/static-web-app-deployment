642c28d9b2d4840d15a704036d05e04bcb541fdb
karwowski1 <131033750+karwowski1@users.noreply.github.com>
2026-04-22 16:57:03 +0200

    change ports

--- 40-jenkins-pt4/terraform/modules/app_cluster/01-alb.tf
+++ 40-jenkins-pt4/terraform/modules/app_cluster/01-alb.tf
@@ -3,8 +3,8 @@ resource "aws_security_group" "alb_sg" {
   vpc_id = var.vpc_id
 
   ingress {
-    from_port   = 443
-    to_port     = 443
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
